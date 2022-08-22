6bbc4a5922ed07117c29447c357bc8f00ae6f508
Gabriel Castro <gabrieldcc@gmail.com>
2022-08-22 13:23:26 -0300

    adiciona webview na AboutScreen

--- BullsEyeApp/Screens/Main/AboutViewController.swift
+++ BullsEyeApp/Screens/Main/AboutViewController.swift
@@ -6,27 +6,24 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let url = Bundle.main.url(
+            forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
+    
+    @IBOutlet weak var webView: WKWebView!
     
     @IBAction func closeButton(_ sender: UIButton) {
         dismiss(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
